4673469581f238fe5435c22a1c416dd6e81f7966
Karen <karen@iamat.com>
2016-06-19 16:47:59 -0300

    add and remove classes of selected to-dos

--- app/app.js
+++ app/app.js
@@ -8,10 +8,41 @@
 
 })(function (tools) {
   // select elements
+  var main = document.querySelector('.container');
   var sendTodoInput = document.querySelector('.container .form input');
   var sendTodoButton = document.querySelector('.container .form button');
   var todosContainer = document.querySelector('.container .todos');
   var checkboxInput = document.querySelector('.container .todos .todo input');
+
+
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.target === todosContainer) {
+        todosContainer = mutation.target;
+        return Array.prototype.map.call(todosContainer.children, function (child) {
+          child.onclick = function() {
+            if (Array.prototype.indexOf.call(this.classList, 'selected') !== -1) {
+              return this.classList = ['todo'];
+            }
+            return this.classList.add('selected');
+          };
+        });
+      }
+      return null;
+    });
+  });
+
+  // configuration of the observer:
+  var config = {
+    childList: true,
+    attributes: true,
+    characterData: true,
+    subtree: true,
+  };
+
+  // pass in the target node, as well as the observer options
+  observer.observe(main, config);
 
   var createTodo = function(text) {
     // create html nodes
@@ -38,10 +69,5 @@
     sendTodoInput.value = '';
   }
 
-  function onCompletedTodo() {
-    console.log('right!')
-  }
-
   sendTodoButton.onclick = onCreateTodo;
-  checkboxInput.onclick = onCompletedTodo;
 });
